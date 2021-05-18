80c3ce12f5d1d2eb454d0aa3b36346cc43f10915
Your Name <you@example.com>
2021-05-18 21:24:25 +0000

    Secondary commit

--- adding.py
+++ adding.py
@@ -2,3 +2,47 @@
 #
 # Created by: Austin de Mora
 # Created on: May 2021
+# This program loops and adds multiple numbers
+
+
+def main():
+    # This function loops and adds multiple numbers
+    # process
+    while True:
+        # Input
+        input_amount_string = input("Enter how many numbers you "
+                                    "want add together: ")
+        try:
+            input_amount = int(input_amount_string)
+            break
+        except Exception:
+            print("This was not an integer")
+
+    number_sum = 0
+    loop_counter = 0
+
+    while loop_counter < input_amount:
+        loop_counter = loop_counter + 1
+
+        # Input
+        while True:
+            chosen_number_string = input("Enter your chosen integer: ")
+
+            try:
+                chosen_number = int(chosen_number_string)
+                chosen_number > 0
+                break
+            except Exception:
+                print("Invalid Input")
+
+        number_sum = number_sum + chosen_number
+
+        if loop_counter < input_amount:
+            continue
+
+        print("")
+        print("The sum of all numbers are: {}".format(number_sum))
+
+
+if __name__ == "__main__":
+    main()
